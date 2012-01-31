fa3c6f4f8934d7268099f7975413ec8da1478324
John Heaton <jheaton.dev@gmail.com>
2012-01-31 14:11:51 -0500

    Initial commit.

---
+++ JRTextFieldCell.h
@@ -0,0 +1,45 @@
+//
+//  JRTextFieldCell.h
+//  Stuff
+//
+//  Created by John Heaton on 1/31/12.
+//  Copyright (c) 2012 GJB Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol TextFieldCellDelegate;
+
+@interface TextFieldCell : UITableViewCell <UITextFieldDelegate> {
+    UITextField                 *_textField;
+    NSUInteger                  _offset;
+    BOOL                        _autoSize;
+    id<TextFieldCellDelegate>   _returnDelegate;
+}
+
+// The text field. Customize as you wish.
+@property (nonatomic, readonly) UITextField *textField;
+
+// The amount to offset the y position of the textField's frame
+@property (nonatomic, assign) NSUInteger textFieldOffset;
+
+// This will automatically align the text field to the right of the textLabel.
+// NOTE: This CAN be used with the textFieldOffset property. It will be added.
+@property (nonatomic, assign) BOOL autoSizesWithLabel;
+
+// Delegate gets a message when you hit return on the text field.
+// Convenient for return chains in tables where there are multiple text cells
+@property (nonatomic, assign) id<TextFieldCellDelegate> returnDelegate;
+
+// Just convenience methods for becoming/resigning first responder
+- (void)beginEditing;
+- (void)endEditing;
+
+@end
+
+
+@protocol TextFieldCellDelegate <NSObject>
+
+- (void)textFieldCellReturned:(TextFieldCell *)textFieldCell;
+
+@end
---
+++ JRTextFieldCell.m
@@ -0,0 +1,92 @@
+//
+//  JRTextFieldCell.m
+//  Stuff
+//
+//  Created by John Heaton on 1/31/12.
+//  Copyright (c) 2012 GJB Software. All rights reserved.
+//
+
+#import "TextFieldCell.h"
+
+@interface TextFieldCell ()
+
+- (void)_applyTextFieldOffset;
+
+@end
+
+@implementation TextFieldCell
+@synthesize textField=_textField;
+@synthesize textFieldOffset=_offset;
+@synthesize autoSizesWithLabel=_autoSize;
+@synthesize returnDelegate=_returnDelegate;
+
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        _textField = [[UITextField alloc] initWithFrame:CGRectZero];
+        self.textField.delegate = self; 
+        
+        self.textFieldOffset = 0;
+        self.autoSizesWithLabel = NO;
+        
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+        [self.contentView addSubview:self.textField];
+    }
+    return self;
+}
+
+- (void)_applyTextFieldOffset {    
+    CGRect frame;
+    frame.origin.x = (10 + self.textFieldOffset);
+    frame.origin.y = ((self.contentView.frame.size.height / 2) - (self.textField.frame.size.height / 2));
+    frame.size.width = ((self.contentView.frame.size.width - 20) - self.textFieldOffset);
+    frame.size.height = self.textField.frame.size.height;
+    if(self.autoSizesWithLabel) {
+        NSInteger offset = ([self.textLabel.text sizeWithFont:self.textLabel.font].width + 5);
+        
+        frame.origin.x += offset;
+        frame.size.width -= offset;
+    }
+    
+    [self.textField setFrame:frame];
+}
+
+- (void)beginEditing {
+    [self.textField becomeFirstResponder];
+}
+
+- (void)endEditing {
+    [self.textField resignFirstResponder];
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [self.textField sizeToFit];
+    [self _applyTextFieldOffset];
+    
+    [self.contentView bringSubviewToFront:self.textField];
+}
+
+- (void)prepareForReuse {
+    self.returnDelegate = nil;
+    self.textFieldOffset = 0;
+    self.autoSizesWithLabel = NO;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if(self.returnDelegate && [self.returnDelegate respondsToSelector:@selector(textFieldCellReturned:)])
+        [self.returnDelegate textFieldCellReturned:self];
+    
+    return YES;
+}
+
+- (void)dealloc {
+    [self.textField release];
+    [super dealloc];
+}
+
+@end
